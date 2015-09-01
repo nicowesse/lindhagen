6eb6699e3e92e1e2e5474219a6748ea3b6c95a88
nicowesse <nico.wesse@gmail.com>
2015-09-01 23:00:44 +0200

    Home-changes

---
+++ sondre/page-templates/home.php
@@ -0,0 +1,82 @@
+<?php
+/**
+ * Template Name: Hjem-loop
+ *
+ * @package nicowesse
+ * @subpackage lindhagen
+ * @since Lindhagen 1.0
+ */
+
+get_header(); ?>
+
+<?php
+// Start the Loop.
+while ( have_posts() ) : the_post();
+	$about = get_the_content();
+?>
+	<header class="large-12 columns main-description">
+		<p><?php bloginfo('description'); ?></p>
+	</header>
+
+
+	<?php
+	if (is_page()) :
+
+		$sticky = get_option( 'sticky_posts' );
+        // first loop to display only my single,
+        // MOST RECENT sticky post
+        $sticky_post = new WP_Query( array(
+            'post__in'            => get_option( 'sticky_posts' ),
+            'ignore_sticky_posts' => 1,
+            'orderby'             => 'date',
+            'posts_per_page'      => 1
+        ) );
+
+        while ( $sticky_post->have_posts() ) :  $sticky_post->the_post();
+        	get_template_part( 'content', 'sticky' );
+    	endwhile;
+    	wp_reset_query();
+
+
+		$cat = 1; //use page title to get a category ID
+		//$posts = get_posts( "cat=$cat&showposts=6" );	
+		$posts = get_posts( array( 
+			'post__not_in' => get_option( 'sticky_posts' ), 
+			'posts_per_page' => 6, 
+			'category' => 1 
+		) );
+
+		if ($posts) :
+	    	foreach ($posts as $post):
+		  		setup_postdata($post);
+
+		  		get_template_part( 'content', get_post_format() );
+
+			endforeach;
+		endif;
+
+	endif;
+	?>
+
+	<div class="large-12 columns explain">
+		<a href="/intervjuer">Vis alle</a>
+	</div>
+
+</section>
+
+<figure class="about-sondre">
+	<figcaption class="about-sondre__overlay">
+		<span class="about-sondre__tagline">Hvem er Sondre?</span>
+	</figcaption>
+</figure>
+
+<article class="row">
+	<div class="large-10 columns large-centered about">
+		<p class="description"><?php echo $about; ?></p>
+	</div>
+</article>
+
+<?php
+endwhile; ?>
+
+<?php get_footer(); ?>
